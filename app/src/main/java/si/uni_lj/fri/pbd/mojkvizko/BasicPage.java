9a5332926341d3e1f1622ae62e0fe4e539a70a9f
sabinca97 <sm3082@student.uni-lj.si>
2021-12-07 19:53:22 +0100

    Init commit01

---
+++ app/src/main/java/si/uni_lj/fri/pbd/mojkvizko/BasicPage.java
@@ -0,0 +1,14 @@
+package si.uni_lj.fri.pbd.mojkvizko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_page);
+    }
+}
